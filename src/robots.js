f1f7bc723f17e978a26f49545aec4397614ac680
rananjaysingh20 <rananjaysingh20@gmail.com>
2022-07-13 10:43:54 +0530

    new files

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Deepanshi',
+		username: 'Deepu',
+		email: 'deepu@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'Devansh',
+		username: 'Dev',
+		email: 'dev@gmail.com'
+	},
+	{
+		id: 3,
+		name: 'Amaan',
+		username: 'momdan',
+		email: 'momdan@gmail.com'
+	},
+	{
+		id: 4,
+		name: 'Dhananjay',
+		username: 'Dheeru',
+		email: 'dheeru@gmail.com'
+	},
+	{
+		id: 5,
+		name: 'Vinay',
+		username: 'Vinny',
+		email: 'vinny@gmail.com'
+	},
+	{
+		id: 6,
+		name: 'Jitendra',
+		username: 'jitu',
+		email: 'jitu@gmail.com'
+	},
+	{
+		id: 7,
+		name: 'Rahul',
+		username: 'dancer',
+		email: 'dancer@gmail.com'
+	},
+	{
+		id: 8,
+		name: 'Ishan',
+		username: 'mote',
+		email: 'mote@gmail.com'
+	}
+];
