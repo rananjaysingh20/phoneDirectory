f311959ecc3e8f855689a14869a0d74e6041f618
rananjaysingh20 <rananjaysingh20@gmail.com>
2022-07-17 18:33:13 +0530

    pushing new

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -9,8 +9,8 @@ const CardList = ( { robots } ) => {
 			robots.map((user,i) => {
 				return (
 					<Card 
-						key={i} 
-						id={robots[i].id} 
+						key={i}
+						id={robots[i].id}
 						name={robots[i].name} 
 						phone={robots[i].phone} 
 						email={robots[i].email}
