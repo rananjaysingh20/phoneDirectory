f2b1b671ddf8e11f90c78a2237d26a9c6db64687
rananjaysingh20 <rananjaysingh20@gmail.com>
2022-07-17 17:52:51 +0530

    changes to database

--- src/robots.js
+++ src/robots.js
@@ -3,42 +3,48 @@ export const robots = [
 		id: 1,
 		name: 'Deepanshi',
 		username: 'Deepu',
-		email: 'deepu@gmail.com',
-		image: 'C:\React\new\roboFriends\src\components\img\Deepu.jpg'
+		email: 'deepu@gmail.com'
 	},
 	{
 		id: 2,
 		name: 'Devansh',
 		username: 'Dev',
-		email: 'dev@gmail.com',
-		image: 'C:\React\new\roboFriends\src\components\img\Deepu.jpg'
+		email: 'dev@gmail.com'
 	},
 	{
 		id: 3,
 		name: 'Amaan',
 		username: 'momdan',
-		email: 'momdan@gmail.com',
-		image: 'C:\React\new\roboFriends\src\components\img\Deepu.jpg'
+		email: 'momdan@gmail.com'
 	},
 	{
 		id: 4,
 		name: 'Dhananjay',
 		username: 'Dheeru',
-		email: 'dheeru@gmail.com',
-		image: 'C:\React\new\roboFriends\src\components\img\Deepu.jpg'
+		email: 'dheeru@gmail.com'
 	},
 	{
 		id: 5,
 		name: 'Amber',
 		username: 'Ambi',
-		email: 'ambi@gmail.com',
-		image: 'C:\React\new\roboFriends\src\components\img\Deepu.jpg'
+		email: 'ambi@gmail.com'
 	},
 	{
 		id: 6,
+		name: 'Rahul',
+		username: 'Dancer',
+		email: 'rahul@gmail.com'
+	},
+	{
+		id: 7,
+		name: 'Vinay',
+		username: 'vinny',
+		email: 'vinny@gmail.com'
+	},
+	{
+		id: 8,
 		name: 'Nazim',
-		username: 'Nazim',
-		email: 'Nazim@gmail.com',
-		image: 'C:\React\new\roboFriends\src\components\img\Deepu.jpg'
+		username: 'nazim',
+		email: 'nazim@gmail.com'
 	}
 ];
