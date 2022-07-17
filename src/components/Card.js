d436cbe3ea3aacafaf66a8d706dea9bfe68d1285
rananjaysingh20 <rananjaysingh20@gmail.com>
2022-07-17 23:45:40 +0530

    photos

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Card = ({name, email, id, phone}) => {
 	return (
+		
 		<div className='tc bg-near-black dib br3 pa1 ma2 grow bw3 shadow-5'>
-			<img src={require('./img/Loading.png')} width="200px" alt='photo'/>
+			<img src={`https://robohash.org/${id}?200x200`} width="200px" alt='photo'/>
 			<div>
 				<h2>{name}</h2>
 				<p>{phone}</p>
